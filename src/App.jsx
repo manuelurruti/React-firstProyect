364989f94a85724d72a4a4e7d2e2a5dc18092b17
manuelurruti <manuelurrutimarquez@gmail.com>
2023-01-03 10:17:51 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
+function App() {
+    return (
+        <div>
+            <header>
+                <h1 className='title'>🎬Peliculas</h1>
+            </header>
+            <MyRoutes />
+        </div>
+    );
+}
+
+export default App;
